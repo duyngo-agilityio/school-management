67359bde67f89b6a6abd129d0b5cc3ad4de4ef09
duy.ngo <duy.ngo@asnet.com.vn>
2024-08-05 10:22:22 +0700

    feat: create mock data

---
+++ src/mocks/students.ts
@@ -0,0 +1,52 @@
+export const MOCK_STUDENTS = [
+  {
+    fullName: "Stacy Prohaska",
+    email: "Isaiah16@gmail.com",
+    gender: 0,
+    age: 16,
+    description: "Error eaque doloribus maiores repudiandae. Doloribus iure fuga blanditiis saepe quibusdam. Quasi suscipit ratione quod. Rem tenetur assumenda tenetur aliquam autem vel nostrum. Laboriosam laborum inventore illo nesciunt vel vel soluta dignissimos ipsam. Molestiae ea labore totam enim.",
+    phoneNumber: "488-713-9023",
+    avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/922.jpg",
+    id: "6"
+    },
+    {
+    fullName: "Victoria Runolfsson",
+    email: "Cortez36@hotmail.com",
+    gender: 0,
+    age: 16,
+    description: "Quia architecto nesciunt impedit vel minus asperiores ad ex. Repudiandae ex enim repellat reiciendis modi laudantium. Sint voluptatum reprehenderit suscipit ea tempora.",
+    phoneNumber: "446-814-2948",
+    avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1233.jpg",
+    id: "7"
+    },
+    {
+    fullName: "Courtney Stroman",
+    email: "Tyrese_Hahn@gmail.com",
+    gender: 0,
+    age: 16,
+    description: "Et placeat harum expedita eveniet cumque. Provident doloribus esse perferendis error ullam facere molestias eius. Magnam incidunt quasi facilis tempore aperiam sapiente. Voluptate explicabo itaque a quasi aliquid. Autem magnam ipsum eius voluptatum commodi.",
+    phoneNumber: "244-577-1884",
+    avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/267.jpg",
+    id: "8"
+    },
+    {
+    fullName: "Daisy Schaden",
+    email: "Jude_Schaefer54@gmail.com",
+    gender: 1,
+    age: 16,
+    description: "Unde occaecati autem corporis sit officiis impedit quidem iste. Nulla molestias reprehenderit repudiandae. Sunt suscipit cupiditate expedita ducimus minus eos aliquid. Omnis itaque ea deleniti corrupti corporis enim quibusdam. Corrupti fugiat alias voluptas dolorum mollitia odit temporibus. Quam optio architecto magni dolore assumenda expedita voluptate culpa.",
+    phoneNumber: "473-516-5777",
+    avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/458.jpg",
+    id: "9"
+    },
+    {
+    fullName: "Michelle Cormier",
+    email: "Courtney_Legros@gmail.com",
+    gender: 1,
+    age: 16,
+    description: "Voluptate voluptates sint quidem modi officia ea. Quisquam tenetur doloribus natus vitae ea est excepturi. Quaerat dolorem expedita itaque sit veniam pariatur laudantium deserunt. Temporibus odit mollitia alias. Asperiores temporibus ratione mollitia cumque.",
+    phoneNumber: "380-976-9633",
+    avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/427.jpg",
+    id: "10"
+    },
+];
---
+++ src/mocks/teachers.ts
@@ -0,0 +1,56 @@
+export const MOCK_TEACHERS = [{
+  fullName: "Naomi Murazik",
+  email: "Therese_Johnston16@hotmail.com",
+  gender: 1,
+  age: 43,
+  description: "Tenetur nemo ab mollitia sapiente odit deserunt. Veritatis autem nihil nisi praesentium eaque quos ab doloremque. Odit et quaerat excepturi unde. Aperiam suscipit enim cumque voluptate.",
+  phoneNumber: "355-641-5320",
+  avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/243.jpg",
+  subject: "Literature",
+  id: "1"
+  },
+  {
+  fullName: "Jan Williamson DDS",
+  email: "Lavinia.Bahringer16@hotmail.com",
+  gender: 0,
+  age: 30,
+  description: "Vitae ipsum repellat natus magnam iusto dolorum optio unde. In cumque eveniet non excepturi. Deleniti tempore illum expedita.",
+  phoneNumber: "264-264-2834",
+  avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/610.jpg",
+  subject: "Physics",
+  id: "2"
+  },
+  {
+  fullName: "Javier Rice Sr.",
+  email: "Lue.Fay90@hotmail.com",
+  gender: 1,
+  age: 35,
+  description: "Illum sapiente explicabo perferendis architecto. Maiores eos fuga nihil exercitationem. Consequatur id ex laudantium iste repudiandae accusamus officiis hic.",
+  phoneNumber: "509-374-7387",
+  avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/588.jpg",
+  subject: "Math",
+  id: "3"
+  },
+  {
+  fullName: "Bonnie Weissnat",
+  email: "Jake_Schamberger@gmail.com",
+  gender: 0,
+  age: 40,
+  description: "Numquam rerum itaque. Vel accusantium eligendi magni architecto nisi molestiae rerum corporis debitis. Pariatur placeat ipsum earum. Porro nobis molestias odio nemo autem eligendi non. Tenetur sint maiores. Voluptas fugit ipsam id suscipit ipsam aut ea animi.",
+  phoneNumber: "706-895-6640",
+  avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/289.jpg",
+  subject: "Biology",
+  id: "4"
+  },
+  {
+  fullName: "Shannon Conroy",
+  email: "Isom.Hettinger95@gmail.com",
+  gender: 0,
+  age: 38,
+  description: "Molestiae veniam quasi consectetur officiis commodi tempora. Nesciunt ut autem dolor provident. Accusamus explicabo veritatis eos alias eum modi labore. Adipisci aperiam voluptas voluptas natus id consectetur ducimus.",
+  phoneNumber: "494-572-1501",
+  avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/100.jpg",
+  subject: "Math",
+  id: "5"
+  },
+];
