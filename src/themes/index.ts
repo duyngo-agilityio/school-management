1162efe86e9f4351322142260afdbc8abc1ca756
duy.ngo <duy.ngo@asnet.com.vn>
2024-08-07 11:37:20 +0700

    feat: update export customFlex

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,10 +1,9 @@
 import { extendTheme } from '@chakra-ui/react';
 
 // Themes
-import { Avatar, Button, Input, Text, Profile } from './components';
+import { Avatar, Button, CustomFlex, Input, Text } from './components';
 import { fonts, fontSizes, radius } from './typographies';
 import { colors } from './colors';
-import { CustomFlex } from './components/customFlex';
 
 const theme = extendTheme({
   colors,
@@ -16,7 +15,6 @@ const theme = extendTheme({
     Input,
     Text,
     Avatar,
-    Profile,
     CustomFlex,
   },
 });
