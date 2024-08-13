206d029a117ce659602c7e36b4d9ae2e835b4841
duy.ngo <duy.ngo@asnet.com.vn>
2024-08-13 17:31:18 +0700

    feat: implement student modal

--- src/components/TableStudent/index.tsx
+++ src/components/TableStudent/index.tsx
@@ -1,9 +1,10 @@
 'use client';
 
 // Components
-import { Button } from '@chakra-ui/react';
+import { Button, useDisclosure } from '@chakra-ui/react';
 import Table from '../Table';
 import Profile from '../Profile';
+import StudentModal from '../Modal/StudentModal';
 
 // Mocks
 import { MOCK_STUDENTS } from '@/mocks';
@@ -14,46 +15,55 @@ import { COLUMNS } from '@/constants';
 // Types
 import { IStudent } from '@/types';
 
-export const COLUMNS_STUDENT = [
-  {
-    field: COLUMNS.FIELDS.NAME,
-    headerName: COLUMNS.HEADER_NAME.NAME,
-    render: (_: unknown, { fullName, avatar }: IStudent) => {
-      return <Profile src={avatar} title={fullName} variant="xs" />;
-    },
-  },
-  {
-    field: COLUMNS.FIELDS.ID,
-    headerName: COLUMNS.HEADER_NAME.STUDENT_ID,
-  },
-  {
-    field: COLUMNS.FIELDS.EMAIL,
-    headerName: COLUMNS.HEADER_NAME.EMAIL,
-  },
-  {
-    field: COLUMNS.FIELDS.CLASS,
-    headerName: COLUMNS.HEADER_NAME.CLASS,
-  },
-  {
-    field: COLUMNS.FIELDS.GENDER,
-    headerName: COLUMNS.HEADER_NAME.GENDER,
-  },
-  {
-    field: COLUMNS.FIELDS.ACTION,
-    headerName: COLUMNS.HEADER_NAME.ACTION,
-    render: () => (
-      <>
-        <Button variant="warning" mr="10px">
-          EDIT
-        </Button>
-        <Button variant="danger">DELETE</Button>
-      </>
-    ),
-  },
-];
-
 const TableStudent = () => {
-  return <Table columns={COLUMNS_STUDENT} data={MOCK_STUDENTS} />;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const COLUMNS_STUDENT = [
+    {
+      field: COLUMNS.FIELDS.NAME,
+      headerName: COLUMNS.HEADER_NAME.NAME,
+      render: (_: unknown, { fullName, avatar }: IStudent) => {
+        return <Profile src={avatar} title={fullName} variant="xs" />;
+      },
+    },
+    {
+      field: COLUMNS.FIELDS.ID,
+      headerName: COLUMNS.HEADER_NAME.STUDENT_ID,
+    },
+    {
+      field: COLUMNS.FIELDS.EMAIL,
+      headerName: COLUMNS.HEADER_NAME.EMAIL,
+    },
+    {
+      field: COLUMNS.FIELDS.CLASS,
+      headerName: COLUMNS.HEADER_NAME.CLASS,
+    },
+    {
+      field: COLUMNS.FIELDS.GENDER,
+      headerName: COLUMNS.HEADER_NAME.GENDER,
+    },
+    {
+      field: COLUMNS.FIELDS.ACTION,
+      headerName: COLUMNS.HEADER_NAME.ACTION,
+      render: () => (
+        <>
+          <Button variant="warning" mr="10px" onClick={onOpen}>
+            EDIT
+          </Button>
+          <Button variant="danger">DELETE</Button>
+        </>
+      ),
+    },
+  ];
+
+  return (
+    <>
+      {isOpen && (
+        <StudentModal isOpen={isOpen} onClose={onClose} title="Add Student" />
+      )}
+      <Table columns={COLUMNS_STUDENT} data={MOCK_STUDENTS} />
+    </>
+  );
 };
 
 export default TableStudent;
