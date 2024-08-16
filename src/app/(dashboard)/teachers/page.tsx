930106aaaf651226df85b082015055a92c2a6905
Duy Hoang The Ngo <duy.ngo@asnet.com.vn>
2024-08-16 14:48:30 +0700

    feat: integerate api get teacher list

--- src/app/(dashboard)/teachers/page.tsx
+++ src/app/(dashboard)/teachers/page.tsx
@@ -1,8 +1,11 @@
 // Components
 import { HeaderBar, SearchTeachers, TableTeacher } from '@/components';
+import { getTeacherList } from '@/services';
 import { Box, Button } from '@chakra-ui/react';
 
-const Teachers = () => {
+const Teachers = async () => {
+  const data = await getTeacherList();
+
   return (
     <Box p="40px">
       <HeaderBar width="full">
@@ -12,7 +15,7 @@ const Teachers = () => {
         <SearchTeachers />
       </Box>
       <Box mt="11px">
-        <TableTeacher />
+        <TableTeacher data={data} />
       </Box>
     </Box>
   );
---
+++ src/constants/env.ts
@@ -0,0 +1,2 @@
+export const TEACHER_URL = process.env.NEXT_PUBLIC_TEACHER_URL;
+export const STUDENT_URL = process.env.NEXT_PUBLIC_STUDENT_URL;
