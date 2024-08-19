9c590d35e008b899b736596e7baae91a50c3adbf
duy.ngo <duy.ngo@asnet.com.vn>
2024-08-19 18:11:54 +0700

    feat: implement unit test for common component

---
+++ src/components/HeaderBar/__test__/HeaderBar.test.tsx
@@ -0,0 +1,25 @@
+import { render } from '@testing-library/react';
+
+// Components
+import HeaderBar from '..';
+
+const HeaderBarProps = {
+  children: <>Content</>,
+  width: '357px',
+};
+
+const HeaderBarComponent = () => render(<HeaderBar {...HeaderBarProps} />);
+
+describe('HeaderBar Component', () => {
+  it('should match snapshot for HeaderBar', () => {
+    const { container } = HeaderBarComponent();
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('applies the correct width to the Flex container', () => {
+    const { container } = HeaderBarComponent();
+
+    expect(container.firstChild).toHaveStyle('width: 357px');
+  });
+});
