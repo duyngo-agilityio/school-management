bc3fc9bb627d768826e6896f6d29bef73c4718a0
duy.ngo <duy.ngo@asnet.com.vn>
2024-08-22 17:53:59 +0700

    feat: create hook for handle submit delete

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,5 +1,1 @@
-// TODO: Remove it later
-export const TEACHER_URL = process.env.NEXT_PUBLIC_TEACHER_URL;
-export const STUDENT_URL = process.env.NEXT_PUBLIC_STUDENT_URL;
-
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
---
+++ src/hooks/useDelete.ts
@@ -0,0 +1,28 @@
+import { useToast } from '@chakra-ui/react';
+
+type DeleteFunc = (id: string) => Promise<boolean>;
+
+interface DeleteOptions {
+  successMessage: string;
+  errorMessage: string;
+  onCloseConfirmModal: () => void;
+}
+
+export const useDelete = (deleteFunc: DeleteFunc, options: DeleteOptions) => {
+  const { errorMessage, successMessage, onCloseConfirmModal } = options;
+
+  const toast = useToast();
+
+  const handleSubmitDelete = async (id: string) => {
+    const isSuccess = await deleteFunc(id);
+
+    onCloseConfirmModal();
+
+    toast({
+      title: isSuccess ? successMessage : errorMessage,
+      status: isSuccess ? 'success' : 'error',
+    });
+  };
+
+  return { handleSubmitDelete };
+};
