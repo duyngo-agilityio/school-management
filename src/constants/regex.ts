b66923983e7b6e13bd2989bfff2f5e24e68b4293
Duy Hoang The Ngo <duy.ngo@asnet.com.vn>
2024-08-28 10:37:07 +0700

    fix: modal should be not change width when have error message from password

--- src/constants/regex.ts
+++ src/constants/regex.ts
@@ -2,3 +2,5 @@ export const REGEX_EMAIL = /^\w+([\\.-]?\w+)*@\w+([\\.-]?\w+)*(\.\w{2,3})+$/;
 
 export const REGEX_PASSWORD =
   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+export const REGEX_PHONE_NUMBER = /^[0-9]{10}$/;
