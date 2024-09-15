115c665091a17a00d259879e46e7d35405fddb5f
duy.ngo <duy.ngo@asnet.com.vn>
2024-09-15 22:00:22 +0700

    feat: add robots and sitmap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,22 @@
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://school-management-admin-dashboard.vercel.app/teachers',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://school-management-admin-dashboard.vercel.app/students',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://school-management-admin-dashboard.vercel.app/signin',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://school-management-admin-dashboard.vercel.app/signup',
+      lastModified: new Date(),
+    },
+  ];
+}
